8551657abb35e2cc7656231b919fedbaf271f11b
yan-shlain <yans@simplex.com>
2021-03-10 15:57:12 +0200

    Reformat

--- user_manager_proj/users_app/serializers.py
+++ user_manager_proj/users_app/serializers.py
@@ -1,7 +1,11 @@
 from rest_framework import serializers
 from .models import User
 
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User # this is the model that is being serialized
+        model = User  # this is the model that is being serialized
         fields = '__all__'
+
+        # For partial serialization un-mark below line
+        # fields = ('id', 'first_name')
